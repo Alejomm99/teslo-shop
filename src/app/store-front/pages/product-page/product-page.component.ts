a054304c8a477e4b492fe71c4cdd7be98c665a04
Alejo <alejomesa.999@gmail.com>
2025-07-30 20:58:00 -0500

    Implement product page with loading state and dynamic product details

--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
 
 @Component({
   selector: 'app-product-page',
   imports: [],
   templateUrl: './product-page.component.html',
 })
-export class ProductPageComponent { }
+export class ProductPageComponent {
+  productIdSlug = inject(ActivatedRoute).snapshot.params['idSlug'];
+
+  productsService = inject(ProductsService);
+
+  productResource = rxResource({
+    request: () => ({ idSlug: this.productIdSlug }),
+    loader: ({ request }) => {
+      return this.productsService.getProductByIdSlug(request.idSlug);
+    },
+  });
+}
