c737c76e44db41c2c75bc62c4c3c50af80f09d2a
Alejo <alejomesa.999@gmail.com>
2025-08-18 20:58:00 -0500

    Add logging interceptor and update app configuration for HTTP client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { loggingInterceptor } from '@shared/interceptors/logging.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([loggingInterceptor])),
   ],
 };
